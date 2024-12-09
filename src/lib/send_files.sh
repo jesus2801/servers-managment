94f54df15a1f7ab0ddce6a7174d8fd3c91128d32
jesus2801 <jesusd280147@gmail.com>
2024-12-09 14:20:21 -0500

    files transfer finished

--- src/lib/send_files.sh
+++ src/lib/send_files.sh
@@ -6,7 +6,25 @@ send_files() {
 
   case "$?" in
   0)
-    echo "hello world"
+    read -p "Enter the destination username: " username
+
+    ip=""
+    while [[ ! $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; do
+      read -p "Enter the destination ip: " ip
+    done
+
+    dir=""
+    while [[ ! -d $dir ]]; do
+      read -p "Folder: " dir
+    done
+
+    scp -rv "$dir" "$username@$ip:/home/$username/"
+
+    if [[ $? -ne 0 ]]; then
+      print_msg "error" "Unexpected error when trying to copy files to $ip"
+    fi
+
+    echo "Files successfully transfered"
     ;;
   esac
 }
